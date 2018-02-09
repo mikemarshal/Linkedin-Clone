58faeb4548df8d12c7ba8c3835aaabd4cd5a55d7
Michael Marshal <mikemarshalman@gmail.com>
2018-02-09 02:57:27 -0600

    added more functionality to body element

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './profile.css';
 
 function Profile(prop) {
   return (
     <div className="Profile-Main">
-      test
+      <img className='Profile-Picture' src={prop.profile.imageUrl} alt="profile pic" />
+      <div className="Profile-Name">
+        <b> {prop.profile.name} </b>
+      </div>
     </div>
   )
 }
